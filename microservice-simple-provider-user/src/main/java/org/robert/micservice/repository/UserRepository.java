e7e8b3a85dff6f837b1ce37a3e6a7314d160736e
admin <admin>
2018-07-31 23:08:11 +0800

    first commit

---
+++ microservice-simple-provider-user/src/main/java/org/robert/micservice/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package org.robert.micservice.repository;
+
+import org.robert.micservice.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
