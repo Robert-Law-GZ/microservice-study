807a9051e0c6b34ce55904000bdf29c090dbf651
luoqiang <65605684>
2018-08-01 10:10:53 +0800

    添加访问验证

--- microservice-consumer-movie/src/main/java/org/robert/microservice/user/controller/MovieController.java
+++ microservice-consumer-movie/src/main/java/org/robert/microservice/user/controller/MovieController.java
@@ -3,10 +3,14 @@ package org.robert.microservice.user.controller;
 import org.robert.microservice.user.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 public class MovieController {
@@ -16,8 +20,16 @@ public class MovieController {
   @Autowired
   private RestTemplate restTemplate;
 
+  @Autowired
+  private DiscoveryClient discoveryClient;
+
   @GetMapping("/user/{id}")
   public User findById(@PathVariable Long id) {
     return this.restTemplate.getForObject(this.userServiceUrl + id, User.class);
+  }
+
+  @GetMapping("/user-instance")
+  public List<ServiceInstance> showInfo(){
+    return  this.discoveryClient.getInstances("MICROSERVICE-PROVIDER-USER");
   }
 }
